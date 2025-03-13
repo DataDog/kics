bdab2a10d1ec3e4ea1d644955c79169e5b6d1d1d
bahar-shah <bahar.shah@datadoghq.com>
2025-03-13 11:23:07 -0400

    remove noisy false positive rule

--- assets/queries/terraform/aws/team_tag_not_present/test/negative0.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "good_example" {
-  ami           = "ami-123456"
-  instance_type = "t2.micro"
-
-  tags = {
-    Team        = "DevOps" # ✅ "Team" tag is present
-    Environment = "Production"
-  }
-}
--- assets/queries/terraform/aws/team_tag_not_present/test/negative1.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_s3_bucket" "good_example" {
-  bucket = "my-bucket"
-
-  tags = {
-    Team = "Security" # ✅ "Team" tag is present
-  }
-}
