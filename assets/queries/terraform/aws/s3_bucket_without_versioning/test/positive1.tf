fa34a6c30da94710ca18679a65a85fbb2f289a55
bahar-shah <bahar.shah@datadoghq.com>
2025-04-08 16:36:17 -0400

    testing

--- assets/queries/terraform/aws/s3_bucket_without_versioning/test/positive1.tf
+++ assets/queries/terraform/aws/s3_bucket_without_versioning/test/positive1.tf
@@ -15,12 +15,10 @@ resource "aws_s3_bucket" "positive1" {
   bucket = "my-tf-test-bucket"
   acl    = "private"
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-
   versioning {
     enabled = false
+  }
+  tags = {
+    Name = "My bucket"
   }
 }
