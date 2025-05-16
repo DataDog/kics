8118ffcd54a3181a7ce251e4eca7e96585a0d8d2
James Lewis <james.lewis@datadoghq.com>
2025-05-16 16:34:24 -0400

    Testing new comment features

--- assets/queries/terraform/aws/imdsv1_is_enabled/test/positive.tf
+++ assets/queries/terraform/aws/imdsv1_is_enabled/test/positive.tf
@@ -2,17 +2,10 @@ resource "aws_instance" "bad_example" {
   ami           = "ami-123456"
   instance_type = "t2.micro"
 
-  metadata_options {
-    http_tokens = "optional" # ❌ Should be "required"
-  }
 }
 
 resource "aws_launch_template" "bad_example" {
   name_prefix   = "example"
   image_id      = "ami-123456"
   instance_type = "t2.micro"
-
-  metadata_options {
-    http_tokens = "optional" # ❌ Should be "required"
-  }
 }
