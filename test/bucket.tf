921964530d59ae9a57292ff12f633cf5a648f6ab
Bahar Shah <bahar.shah@datadoghq.com>
2025-06-03 13:29:07 -0400

    Create bucket.tf

---
+++ test/bucket.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.2.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-example-bucket-2"
+}
+
+resource "aws_s3_bucket" "examplee" {
+  bucket = "my-tf-example-bucket"
+}
