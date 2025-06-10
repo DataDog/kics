a1ae5aa2c11a78f7a6b4b71f772d288fab2b5873
Bahar Shah <bahar.shah@datadoghq.com>
2025-06-10 11:42:40 -0400

    Update bucket.tf

--- bucket.tf
+++ bucket.tf
@@ -11,6 +11,10 @@ provider "aws" {
   # Configuration options
 }
 
+resource "aws_s3_bucket" "example3" {
+  bucket = "my-tf-example-bucket-3"
+}
+
 resource "aws_s3_bucket" "example4" {
   bucket = "my-tf-example-bucket-4"
 }
