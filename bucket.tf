49557bf05930a1024da52fd8ffadff705a9217a0
Bahar Shah <bahar.shah@datadoghq.com>
2025-06-17 12:23:24 -0400

    Update bucket.tf

--- bucket.tf
+++ bucket.tf
@@ -7,10 +7,10 @@ terraform {
   }
 }
 
-provider "aws" {
-  # Configuration options
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-example-bucket"
 }
 
-resource "aws_s3_bucket" "example2" {
-  bucket = "my-tf-example-bucket2"
+provider "aws" {
+  # Configuration options
 }
