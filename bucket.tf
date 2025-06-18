a45cc95d1c7b1d2bae918e8f9c18a5d46e5a3101
bahar-shah <bahar.shah@datadoghq.com>
2025-06-18 09:49:24 -0400

    bump

--- bucket.tf
+++ bucket.tf
@@ -7,10 +7,10 @@ terraform {
   }
 }
 
-provider "aws" {
-  # Configuration options
+resource "aws_s3_bucket" "example2" {
+  bucket = "my-tf-example-bucket2"
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-example-bucket"
+provider "aws" {
+  # Configuration options
 }
