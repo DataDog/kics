00f35979c7d0df7361b3a05b127fe2c817928af9
bahar-shah <bahar.shah@datadoghq.com>
2025-06-25 09:26:18 +0200

    add test bucket

---
+++ bucket.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.2.0"
+    }
+  }
+}
+
+resource "aws_s3_bucket" "example2" {
+  bucket = "my-tf-example-bucket2"
+  versioning {
+    enabled = false
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
