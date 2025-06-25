694781c71c1d5066252771c96d67fd3324bb9d80
Bahar Shah <bahar.shah@datadoghq.com>
2025-06-25 13:41:43 +0200

    Update bucket.tf

--- bucket.tf
+++ bucket.tf
@@ -1,16 +1,23 @@
-resource "aws_s3_bucket" "example4" {
-  bucket = "my-tf-example-bucket4"
-  versioning {
-    enabled = false
-  }
-}
-
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "4.2.0"
     }
+  }
+}
+
+resource "aws_s3_bucket" "example3" {
+  bucket = "my-tf-example-bucket4"
+  versioning {
+    enabled = false
+  }
+}
+
+resource "aws_s3_bucket" "example6" {
+  bucket = "my-tf-example-bucket4"
+  versioning {
+    enabled = false
   }
 }
 
