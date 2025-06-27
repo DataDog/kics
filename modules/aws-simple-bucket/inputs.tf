73dcf99d60090b2f938aa3ef17a73edd600b7ab3
Akim Sadaoui <akim.sadaoui@datadoghq.com>
2025-06-27 09:17:07 +0200

    akim- add variable to control versioning in bucket test

--- modules/aws-simple-bucket/inputs.tf
+++ modules/aws-simple-bucket/inputs.tf
@@ -2,3 +2,8 @@ variable "bucket_name" {
   description = "The name of the bucket"
   type        = string
 }
+
+variable "turn_on_versioning" {
+  description = "Whether we activate versioning on this bucket"
+  type        = bool
+}
--- modules/aws-simple-bucket/main.tf
+++ modules/aws-simple-bucket/main.tf
@@ -1,5 +1,10 @@
 resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
+
+  versioning {
+    enabled = var.turn_on_versioning
+  }
+
   tags = {
     "terraform.managed" = "true"
   }
