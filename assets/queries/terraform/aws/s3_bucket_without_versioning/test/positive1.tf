12e75599973eac4a5560b1e13f60d270ccb9fdde
Akim Sadaoui <akim.sadaoui@datadoghq.com>
2025-06-27 09:17:07 +0200

    akim- add variable to control versioning in bucket test

--- assets/queries/terraform/aws/s3_bucket_without_versioning/test/positive1.tf
+++ assets/queries/terraform/aws/s3_bucket_without_versioning/test/positive1.tf
@@ -2,6 +2,7 @@ module "mybucket" {
   source = "../../../../../../modules/aws-simple-bucket"
 
   bucket_name = "dd-mybucket-eric-aws-simple-test-001"
+  turn_on_versioning = false
 }
 
 provider "aws" {
