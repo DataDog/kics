9eb6fb7925c9ac3211597e136399c7a803408709
Benjamin Chouraqui <benjamin.chouraqui@datadoghq.com>
2025-07-02 10:18:43 +0200

    feat: added tests for get_code_snippets function
    Refs: K9VULN-5659

--- documentation/test/test_get_code_snippets.py
+++ documentation/test/test_get_code_snippets.py
@@ -1,17 +1,17 @@
 import pytest
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch
 from pathlib import Path
 
-from documentation.local_gen import get_code_snippets
+from local_gen import get_code_snippets
 
 class TestGetCodeSnippets:
     @pytest.fixture
-    def fake_test_dir():
+    def fake_test_dir(self):
         return Path("/fake/test/dir")
 
-    @patch('documentation.local_gen.read_file_contents')
-    @patch('documentation.local_gen.glob.iglob')
-    def test_get_code_snippets(mock_iglob, mock_read, fake_test_dir):
+    @patch('local_gen.read_file_contents')
+    @patch('local_gen.glob.iglob')
+    def test_get_code_snippets(self, mock_iglob, mock_read, fake_test_dir):
         
         mock_files_neg = [fake_test_dir / "negative1.tf", fake_test_dir / "negative2.tf"]
         mock_files_pos = [fake_test_dir / "positive1.tf"]
