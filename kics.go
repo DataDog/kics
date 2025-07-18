81ffb98236d66bf7dae2589504a7991099505351
Benjamin Chouraqui <benjamin.chouraqui@datadoghq.com>
2025-07-18 10:22:08 +0200

    test for checkov disc

--- kics.go
+++ kics.go
@@ -4,7 +4,7 @@
  * This product includes software developed at Datadog (https://www.datadoghq.com)  Copyright 2024 Datadog, Inc.
  */
 
-package kics
+package main
 
 import (
 	"path/filepath"
@@ -48,4 +48,21 @@ func ExecuteKICSScan(inputPaths []string, outputPath string, sciInfo model.SCIIn
 
 	resultsFile := filepath.Join(outputPath, params.OutputName)
 	return metadata, resultsFile, nil
+}
+
+func main() {
+	inputPaths := []string{"/Users/benjamin.chouraqui/repos/cloud-inventory/"}
+	outputPath := "/Users/benjamin.chouraqui/repos/kics/"
+	sci := model.SCIInfo{
+		DiffAware: model.DiffAware{
+			Enabled: false,
+		},
+		RunType: "code_update",
+		RepositoryCommitInfo: model.RepositoryCommitInfo{
+			RepositoryUrl: "github.com/blah",
+			Branch:        "main",
+			CommitSHA:     "1234567890",
+		},
+	}
+	ExecuteKICSScan(inputPaths, outputPath, sci)
 }
