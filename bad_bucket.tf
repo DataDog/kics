4ca6ae7e2f4e8541e0aa3944037b8c7f6b3af902
Valery Neira <valeryjuli@gmail.com>
2025-08-04 18:00:24 +0200

    Create bad_bucket.tf

---
+++ bad_bucket.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.2.0"
+    }
+  }
+}
+
+resource "aws_s3_bucket" "example4" {
+  bucket = "my-tf-example-bucket4"
+  versioning {
+    enabled = false
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
